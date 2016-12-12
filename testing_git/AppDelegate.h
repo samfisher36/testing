bc4d7111e652a542e1194ccb1266afcbfecf28ed
WeWave <wewave@miteshs-mac-mini.local>
2016-12-12 15:38:56 +0530

    Initial Commit

---
+++ testing_git/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  testing_git
+//
+//  Created by WeWave on 12/12/16.
+//  Copyright © 2016 WeWave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ testing_git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testing_git
+//
+//  Created by WeWave on 12/12/16.
+//  Copyright © 2016 WeWave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
